f0fd95608001ce356680c3fad78e2e3f055d28db
Jakub Hazik <hazik.jakub@gmail.com>
2020-03-14 13:07:38 +0100

    create task manager client

--- amr_task_manager/include/amr_task_manager/Client/TaskManagerClient.h
+++ amr_task_manager/include/amr_task_manager/Client/TaskManagerClient.h
@@ -6,13 +6,43 @@
 #define PROJECT_TASKMANAGERCLIENT_H
 
 #include <ros/ros.h>
+#include <amr_msgs/GetTask.h>
+#include <amr_msgs/ResetTask.h>
+#include <actionlib/client/simple_action_client.h>
+#include <amr_msgs/PerformGoalsAction.h>
+
+
+typedef actionlib::SimpleActionClient<amr_msgs::PerformGoalsAction> PerformWaypoints;
+
 
 class TaskManagerClient {
 public:
     TaskManagerClient(ros::NodeHandle& nh);
 
 private:
+    ros::NodeHandle nh;
+    ros::ServiceClient getTaskSrvClient;
+    ros::ServiceServer resetTaskSrvServer;
+    PerformWaypoints performWaypointsAc;
+    ros::Timer getNewTaskTimer;
 
+    // callbacks
+    bool resetTaskServiceCb(amr_msgs::ResetTask::Request& req, amr_msgs::ResetTask::Response& res);
+
+    void acWaypointsDoneCb(const actionlib::SimpleClientGoalState& state,
+                           const amr_msgs::PerformGoalsResultConstPtr& result);
+
+    void acWaypointsFeedbackCb(const amr_msgs::PerformGoalsFeedbackConstPtr& feedback);
+
+    void activeCallback();
+
+
+    // other methods
+    bool getNewTask();
+
+    bool performTask(amr_msgs::Task& task);
+
+    void callGetNewTaskServiceAfterTime(double timeout);
 
 };
 
