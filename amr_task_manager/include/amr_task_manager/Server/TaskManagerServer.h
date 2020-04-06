e705bda319aaebb296753bff1897e3dc910277a1
Jakub Hazik <hazik.jakub@gmail.com>
2020-04-06 17:51:33 +0200

    improvements in task manager, add custom task call asap

--- amr_task_manager/include/amr_task_manager/Server/TaskManagerServer.h
+++ amr_task_manager/include/amr_task_manager/Server/TaskManagerServer.h
@@ -10,34 +10,58 @@
 
 #include <ros/ros.h>
 #include <amr_msgs/Task.h>
-
 #include <amr_msgs/Node.h>
 #include <amr_msgs/GetTask.h>
 #include <amr_msgs/ResetTask.h>
+#include <amr_msgs/DoCustomTaskAsap.h>
 
-
-class Task {
-public:
-    enum class Commnands {
-        PLAN_PATH,
-        WAIT_FOR_USER_ACK,
-        CHARGE_BATTERY,
-        DO_NOTHING
-    };
-
-    Commnands command;
-    std::pair<amr_msgs::Node, amr_msgs::Node> path;
-    double timeout;
-};
 
 class Client {
 public:
-    explicit Client(const std::string& clientName) {
+    Client() = default;
+    explicit Client(const std::string& clientName)
+        :   clientName(clientName) {
         ros::NodeHandle nh("/");
-        resetSrvServer = nh.serviceClient<amr_msgs::ResetTask>(clientName + "/task_manager_client/reset_task");
+        resetTaskSrv = nh.serviceClient<amr_msgs::ResetTask>(clientName + "/task_manager_client/reset_task");
     }
-    std::queue<Task> tasks;
-    ros::ServiceClient resetSrvServer;
+
+    void doCustomTaskAsap(const amr_msgs::Task& task, bool resumePreviousTask) {
+        if (resumePreviousTask) {
+            tasks.push_front(currentPerformingTask);
+        }
+
+        tasks.push_front(task);
+        resetCurrentTask();
+    }
+
+    bool isNewTaskAvailable() {
+        return !tasks.empty();
+    }
+
+    void addNewTask(const amr_msgs::Task& task) {
+        tasks.push_back(task);
+    }
+
+    amr_msgs::Task getNewTask() {
+        currentPerformingTask = tasks.front();
+        tasks.pop_front();
+        return currentPerformingTask;
+    }
+
+    std::pair<bool, std::string> resetCurrentTask() {
+        amr_msgs::ResetTask srv;
+        resetTaskSrv.call(srv);
+        if (!srv.response.success) {
+            ROS_ERROR("Client %s reset task: %s", clientName.c_str(), srv.response.message.c_str());
+        }
+        return {srv.response.success, srv.response.message};
+    }
+
+    amr_msgs::Task currentPerformingTask;
+private:
+    ros::ServiceClient resetTaskSrv;
+    std::string clientName;
+    std::list<amr_msgs::Task> tasks;
 };
 
 
@@ -47,21 +71,17 @@ typedef std::map<std::string, Client> RobotClients;
 class TaskManagerServer {
 public:
     TaskManagerServer(ros::NodeHandle& nh);
-
-
-
 private:
-
     ros::ServiceServer getTaskSrvServer;
+    ros::ServiceServer pauseClientTask;
     ros::ServiceClient planPathSrvClient;
 
     RobotClients clients;
 
-
     bool getTaskCb(amr_msgs::GetTask::Request& req, amr_msgs::GetTask::Response& res);
+    bool doCustomTaskAsapCb(amr_msgs::DoCustomTaskAsap::Request& req, amr_msgs::DoCustomTaskAsap::Response& res);
 
-
-
+    Client getClient(const std::string& id);
 };
 
 
